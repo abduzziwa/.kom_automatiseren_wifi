c313f0f959790f09e403c85343c4d0db687670c1
abduzziwa <143699732+abduzziwa@users.noreply.github.com>
2025-11-11 23:28:34 +0100

    Register models with custom admin configurations
    Registered multiple models with custom admin classes for better management in Django admin.

--- radapp/admin.py
+++ radapp/admin.py
@@ -1,5 +1,76 @@
 from django.contrib import admin
-from .models import Radcheck
+from .models import (
+    Radcheck, Radreply, Radgroupcheck, Radgroupreply,
+    Radusergroup, Radacct, Radpostauth, Nas
+)
 
-# Register the Radcheck model so it appears in Django admin
-admin.site.register(Radcheck)
+@admin.register(Radcheck)
+class RadcheckAdmin(admin.ModelAdmin):
+    list_display = ('username', 'attribute', 'op', 'value')
+    search_fields = ('username',)
+    inlines = []
+
+
+@admin.register(Radreply)
+class RadreplyAdmin(admin.ModelAdmin):
+    list_display = ('username', 'attribute', 'value')
+    search_fields = ('username__username',)
+
+
+@admin.register(Radgroupcheck)
+class RadgroupcheckAdmin(admin.ModelAdmin):
+    list_display = ('groupname', 'attribute', 'value')
+    search_fields = ('groupname',)
+
+
+@admin.register(Radgroupreply)
+class RadgroupreplyAdmin(admin.ModelAdmin):
+    list_display = ('groupname', 'attribute', 'value')
+    search_fields = ('groupname',)
+
+
+@admin.register(Radusergroup)
+class RadusergroupAdmin(admin.ModelAdmin):
+    list_display = ('username', 'groupname', 'priority')
+    search_fields = ('username__username', 'groupname__groupname')
+
+
+@admin.register(Radacct)
+class RadacctAdmin(admin.ModelAdmin):
+    list_display = (
+        'username', 'nasipaddress', 'acctstarttime',
+        'acctstoptime', 'acctsessiontime', 'acctterminatecause'
+    )
+    search_fields = ('username__username', 'acctsessionid', 'nasipaddress')
+    list_filter = ('acctterminatecause',)
+    readonly_fields = [f.name for f in Radacct._meta.fields]
+
+    # Make read-only (log data)
+    def has_add_permission(self, request):
+        return False
+    def has_change_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Radpostauth)
+class RadpostauthAdmin(admin.ModelAdmin):
+    list_display = ('username', 'reply', 'authdate')
+    search_fields = ('username__username',)
+    list_filter = ('reply',)
+    readonly_fields = [f.name for f in Radpostauth._meta.fields]
+
+    # Make read-only (log data)
+    def has_add_permission(self, request):
+        return False
+    def has_change_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Nas)
+class NasAdmin(admin.ModelAdmin):
+    list_display = ('nasname', 'shortname', 'type', 'description')
+    search_fields = ('nasname', 'shortname')
